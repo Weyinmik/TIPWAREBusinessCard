5d8a6664d99d2eedd9327bb6cafa5db4425a26b3
Esther Weyinmi Koloh <kolohw@gmail.com>
2018-02-20 01:50:34 +0100

    Initial Commit 7 - helpful resources

--- app/src/main/java/com/example/koloh/tipwarebusinesscard/MainActivity.java
+++ app/src/main/java/com/example/koloh/tipwarebusinesscard/MainActivity.java
@@ -73,3 +73,4 @@ public class MainActivity extends Activity {
 
 // Helpful Resources
 //1. website Icon made by <a href="https://www.flaticon.com/authors/smashicons" title="Smashicons">Smashicons</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
+//2. https://stackoverflow.com/questions/21720640/sending-email-from-android-app-when-click-on-button
